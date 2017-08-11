6b512e73925e116fff6d04aaee9fddc53108e02d
Unknown <bo4arovpavel1989@mail.ru>
2017-08-11 09:44:26 +0300

    made ajax reqeust by random

--- index.js
+++ index.js
@@ -4,26 +4,38 @@ $(document).ready(function(){
 
 var myForm={
 	validate: function(){
-			var objectToReturn={errorFields:[]};
-		    var form=document.forms.myForm
+			var objectToReturn={
+				errorFields:[],
+				isValid: false
+			};
+			$('input').each(function(){
+				$(this).removeClass('error');
+			});
+		    var form=document.forms.myForm;
 			var fio = form.elements.fio.value;
 			var email = form.elements.email.value;
 			var phone = form.elements.phone.value;
-			if(fio.match(/^[A-Za-zА-Яа-яЁё]{1,}\s[A-Za-zА-Яа-яЁё]{1,}\s[A-Za-zА-Яа-яЁё]{1,}$/)==null) objectToReturn.errorFields.push('#fio');
-			if (phone.match(/^\+7\(\d\d\d\)\d\d\d-\d\d-\d\d$/)==null) objectToReturn.errorFields.push('#tel');
+			var regexps={
+				fio:/^[A-Za-zА-Яа-яЁё]{1,}\s[A-Za-zА-Яа-яЁё]{1,}\s[A-Za-zА-Яа-яЁё]{1,}$/,
+				email:/^\w{1,}@(ya|yandex)\.(ru|ua|kz|by|com)$/,
+				phone:/^\+7\(\d\d\d\)\d\d\d-\d\d-\d\d$/
+				};
+			if(fio.match(regexps.fio)==null) objectToReturn.errorFields.push('#fio');
+			if(email.match(regexps.email)==null) objectToReturn.errorFields.push('#email');
+			if (phone.match(regexps.phone)==null) objectToReturn.errorFields.push('#phone');
 			else {
 				var phoneArray=phone.split('');
 				var sum=0;
 				phoneArray.forEach(function(item){
-					console.log(isNaN(parseInt (item)))
 					if (!isNaN(parseInt (item))) sum += parseInt (item);
-					
 				});
-				console.log(sum);
-				if(sum>30) objectToReturn.errorFields.push('#tel');
+				if(sum>30) objectToReturn.errorFields.push('#phone');
 			}
-			if(email.match(/^\w{1,}@(ya|yandex)\.(ru|ua|kz|by|com)$/)==null) objectToReturn.errorFields.push('#email');
-			console.log(objectToReturn);
+			objectToReturn.errorFields.forEach(function(field){
+				$(field).addClass('error');
+			});
+			if(objectToReturn.errorFields==0)objectToReturn.isValid=true;
+			return objectToReturn;
 	},
 	getData:function(){
 		
@@ -34,7 +46,20 @@ var myForm={
 	submit: function(){
 		$('#myForm').on('submit',function(e){
 			e.preventDefault();
-			myForm.validate();
+			var answer=myForm.validate();
+			if(answer.isValid){
+				document.getElementById('submitButton').disabled = true;
+				var getRandomInt = function (min, max){return Math.floor(Math.random() * (max - min + 1)) + min;};
+				var queries=['queries/success.json','queries/error.json','queries/progress.json'];
+				var queryIndex=getRandomInt(0,2);
+				$.ajax({
+					url:queries[queryIndex],
+					dataType: 'json',
+					success:function(answer){
+						console.log(answer.status);
+					}
+				});
+			}
 		});
 	}
 };
