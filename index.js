b9e11d015bf0ca731a075af272d9f7006a7db656
Unknown <bo4arovpavel1989@mail.ru>
2017-08-11 15:21:17 +0300

    made all but need to fix for ie

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 $(document).ready(function(){
 	myForm.submit();
+	$('#getData').on('click',function(){
+		console.log(myForm.getData());
+	});
+	$('#setData').on('click',function(){
+		myForm.setData({
+			fio:"qwe qwe qqw",
+			email:"qwe@ya.ru",
+			phone:"+7(111)111-11-11"
+		});
+	});
 });
 
 var myForm={
@@ -11,24 +21,24 @@ var myForm={
 			$('input').each(function(){
 				$(this).removeClass('error');
 			});
-			var fio = $('#fio').val();
-			var email = $('#email').val()
-			var phone = $('#phone').val();
+			var fio = $('#myForm>input[name="fio"]').val();
+			var email = $('#myForm>input[name="email"]').val()
+			var phone = $('#myForm>input[name="phone"]').val();
 			var patterns={
 				fio:/^[A-Za-zА-Яа-яЁё]{1,}\s[A-Za-zА-Яа-яЁё]{1,}\s[A-Za-zА-Яа-яЁё]{1,}$/,
 				email:/^\w{1,}@(ya|yandex)\.(ru|ua|kz|by|com)$/,
 				phone:/^\+7\(\d\d\d\)\d\d\d-\d\d-\d\d$/
 				};
-			if(fio.match(patterns.fio)==null) objectToReturn.errorFields.push('#fio');
-			if(email.match(patterns.email)==null) objectToReturn.errorFields.push('#email');
-			if (phone.match(patterns.phone)==null) objectToReturn.errorFields.push('#phone');
+			if(fio.match(patterns.fio)==null) objectToReturn.errorFields.push('#myForm>input[name="fio"]');
+			if(email.match(patterns.email)==null) objectToReturn.errorFields.push('#myForm>input[name="email"]');
+			if (phone.match(patterns.phone)==null) objectToReturn.errorFields.push('#myForm>input[name="phone"]');
 			else {
 				var phoneArray=phone.split('');
 				var sum=0;
 				phoneArray.forEach(function(item){
 					if (!isNaN(parseInt (item))) sum += parseInt (item);
 				});
-				if(sum>30) objectToReturn.errorFields.push('#phone');
+				if(sum>30) objectToReturn.errorFields.push('#myForm>input[name="phone"]');
 			}
 			objectToReturn.errorFields.forEach(function(field){
 				$(field).addClass('error');
@@ -37,9 +47,9 @@ var myForm={
 			return objectToReturn;
 	},
 	getData:function(){
-			var fio = $('#fio').val();
-			var email = $('#email').val()
-			var phone = $('phone').val();
+			var fio = $('#myForm>input[name="fio"]').val();
+			var email = $('#myForm>input[name="email"]').val()
+			var phone = $('#myForm>input[name="phone"]').val();
 			var objectToReturn={
 				fio:fio,
 				email:email,
@@ -48,7 +58,9 @@ var myForm={
 			return objectToReturn;
 	},
 	setData: function(object){
-		
+		$('#myForm>input[name="fio"]').val(object.fio);
+		$('#myForm>input[name="email"]').val(object.email)
+		$('#myForm>input[name="phone"]').val(object.phone);
 	},
 	submit: function(){
 		$('#myForm').on('submit',function(e){
@@ -69,6 +81,9 @@ var myForm={
 						if(answer.status=='success')$('#resultContainer').append('Success');
 						if(answer.status=='error')$('#resultContainer').append(answer.reason);
 						if(answer.status=='progress')setTimeout(function(){$('#myForm').submit()},answer.timeout);
+					},
+					error:function(e){
+						console.log(e);
 					}
 				});
 			}
